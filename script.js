cf5b351f57f2b34d1d522d74f983a0b7e66a28d5
Yann377 <74406133+Yann377@users.noreply.github.com>
2021-02-14 22:46:38 +0100

    creation page projets

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   $(window).scroll(function(){
     if(this.scrollY > 20){
       $(".navbar").addClass("navmouvement");
+      $(".navbar2").addClass("navmouvement");
     }
     else{
       $(".navbar").removeClass("navmouvement");
+      $(".navbar2").removeClass("navmouvement");
     }
   });
 	});
